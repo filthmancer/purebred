cce9c7037f95126f65bd3cf82a68aac247a6fa7e
matt <matthewhorton@tinmangames.com.au>
2023-11-21 23:56:27 +1100

    scene changes

--- addons/pluginloader/PluginLoader.gd
+++ addons/pluginloader/PluginLoader.gd
@@ -5,7 +5,6 @@ const EditorLinePlugin = preload("res://Scripts/EditorLine.gd")
 var plugin = EditorLinePlugin.new();
 
 func _init():
-	print("ENTER TREE")
 	add_node_3d_gizmo_plugin(plugin);
 	
 func _exit_tree():
