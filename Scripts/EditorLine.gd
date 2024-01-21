7d2d76ce642cb478f59cc1fb85da19fff35ad384
matt <maxerj@gmail.com>
2024-01-22 10:15:48 +1100

    attempting fixup of editor lines

--- Scripts/EditorLine.gd
+++ Scripts/EditorLine.gd
@@ -1,6 +1,6 @@
 @tool
 extends EditorNode3DGizmoPlugin
-const ServerNode = preload("res://Scripts/Server/ServerNode.cs")
+const ServerNode = preload("res://Scripts/Network/ServerNode.cs")
 
 func _get_gizmo_name():
 	return "EditorLine"
@@ -8,6 +8,10 @@ func _get_gizmo_name():
 func _init():
 	create_material("main", Color(1,1,1))
 	create_handle_material("handles")
+	
+func _set_handle(gizmo, handle_id, secondary, camera, screen_pos):
+	#print("HII");
+	_redraw(gizmo);
 	
 func _has_gizmo(for_node_3d):
 	return for_node_3d is ServerNode
@@ -18,16 +22,13 @@ func _redraw(gizmo):
 	var node3d = gizmo.get_node_3d()
 	
 	var lines = PackedVector3Array()
+	var handles = PackedVector3Array()
 	
 	for l in node3d.linked_nodes:
 		lines.push_back(Vector3.ZERO);
 		lines.push_back(l.position - node3d.position);
-
-	
-	#var handles = PackedVector3Array()
-	#handles.push_back(Vector3(0,1,0))
-	#handles.push_back(Vector3(0, 20,0))
+		handles.push_back(l.position - node3d.position);
 	
 	gizmo.add_lines(lines, get_material("main",gizmo), true);
-	#gizmo.add_handles(handles, get_material("handles", gizmo), []);
-		
+	gizmo.add_handles(handles, get_material("handles", gizmo), []);
+
--- addons/pluginloader/PluginLoader.gd
+++ addons/pluginloader/PluginLoader.gd
@@ -3,9 +3,19 @@ extends EditorPlugin
 
 const EditorLinePlugin = preload("res://Scripts/EditorLine.gd")
 var plugin = EditorLinePlugin.new();
+var inspector : EditorInspector
 
-func _init():
+func _enter_tree():
+	inspector = self.get_editor_interface().get_inspector();
 	add_node_3d_gizmo_plugin(plugin);
-	
+
 func _exit_tree():
 	remove_node_3d_gizmo_plugin(plugin);
+	
+# Prevents the InputEvent from reaching other Editor classes.
+func _forward_3d_gui_input(camera, event):
+	#plugin.update();
+	return EditorPlugin.AFTER_GUI_INPUT_PASS
+
+func _handles(object):
+	return true
